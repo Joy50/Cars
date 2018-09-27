29002ed6ef0e5f18ee93083002d5392c10f24886
Joy <joybaust50@gmail.com>
2018-09-27 12:43:55 +0600

    Frist commit
    added

---
+++ Cars/Assets/Scripts/levelloder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelloder : MonoBehaviour {
+    GameObject menu_Canvas;
+    GameObject settings_Canvas;
+	void Start () {
+        menu_Canvas = GameObject.Find("MainMenuCanvas");
+        settings_Canvas = GameObject.Find("SettingCanvus");
+        settings_Canvas.SetActive(false);
+        menu_Canvas.SetActive(true);
+		
+	}
+    public void LevelLoder(int a)
+    {
+        //Application.LoadLevel(a);
+        SceneManager.LoadScene(a);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void loadMenu()
+    {
+        settings_Canvas.SetActive(false);
+        menu_Canvas.SetActive(true);
+    }
+    public void loadSettings()
+    {
+        settings_Canvas.SetActive(true);
+        menu_Canvas.SetActive(false);
+    }
+}
